c8ecbba8a0ecad72903b355644f887d76b16e005
Shelvacu Vevevende <shelvacu@gmail.com>
2013-06-23 11:07:51 -0700

    Working version, including gif generator

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ MathImage
 =========
 
 Inspired by/idea copied from: https://github.com/ichub/MathCanvas
+
+Beware the source code! It's a bit sloppy. You have been warned.
--- gif.rb
+++ gif.rb
@@ -29,7 +29,7 @@ end
 # $img = ChunkyPNG::Image.new(width,height)
 $thingy = (1/256.0)*QuantumRange
 threads = []
-frames = Array.new(num_frames)
+frames = ImageList.new
 num_frames.times.with_progress("Making image") do |t|
   #if threads.length >= 4
   #  threads.first.join
@@ -37,7 +37,7 @@ num_frames.times.with_progress("Making image") do |t|
   #threads << Thread.new(t) do |t|
   i = t
   t = t.to_f + 1
-  frame = []
+  frame = Image.new(width,height)
   width.times do |x|
     x = x.to_f + 1
     height.times do |y|
@@ -45,17 +45,19 @@ num_frames.times.with_progress("Making image") do |t|
       r = r(x,y,t).to_i % 256
       g = g(x,y,t).to_i % 256
       b = b(x,y,t).to_i % 256
+      frame.color_point(x,y,Pixel.new(r,g,b,0))
       #puts 'omg' if r == 0 && g == 0 && b == 0
       #frame += [r,g,b]
-      frame << r*256
-      frame << g*256
-      frame << b*256
+      #frame << r*256
+      #frame << g*256
+      #frame << b*256
       # $img[x,y] = ChunkyPNG::Color.rgb(r,g,b)
     end
     #end
     frames[i]= frame
   end
 end
+=begin
 puts "Processing"
 
 final_gif = ImageList.new
@@ -64,7 +66,7 @@ frames.each do |frame|
   img.import_pixels(0,0,width,height,"RGB",frame)
   final_gif << img
 end
-
+=end
 #raise("#{filename} already exists!") if File.exist?(filename)
 puts 'saving...'
-final_gif.write(filename)
+frames.write(filename)
